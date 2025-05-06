7bf47840a9d536eeb64db4926e6f33d3133ea484
Saakshhdoesnotcode <Saakshi.salvi22@vit.edu>
2025-05-06 22:06:38 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Function to calculate simple interest
+calculate_simple_interest() {
+  principal=$1
+  rate=$2
+  time=$3
+  interest=$(echo "$principal * $rate * $time / 100" | bc)
+  echo "The simple interest is: $interest"
+}
+
+# Example usage
+calculate_simple_interest 1000 5 2
 
